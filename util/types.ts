df3572d9d6a438c75847a32e8bbfcd499d6a1249
jerren <jerren.schexnider@icloud.com>
2025-12-24 12:02:50 -0500

    added readable documentation (i think)

--- util/types.ts
+++ util/types.ts
@@ -1,4 +1,9 @@
-
+/**
+ * position type usable for kaptain
+ * @property {number} x - x axis
+ * @property {number} y - y axis
+ * @property {number} z - z axis
+ */
 export type pos = {
     x: number;
     y: number;
@@ -6,15 +11,18 @@ export type pos = {
 };
 /**
  * raw position from whatever its fetched from
- * @typedef {[x: number, y: number, z: number, e: number]} rawposition
  * @property {number} 0 - x axis coord
  * @property {number} 1 - y axis coord
  * @property {number} 2 - z axis coord
  * @property {number} 3 - extruder whatever it is
  */
 export type raw = [number, number, number, number];
-//lambda converts raw data from the API/ws to usable data in the type of pos
-//i know it seems overengineered, but i realized its an easy fix
+
+/**
+ * turns moonraker's data -> usable data 4 kaptain
+ * @param {raw} arr - raw position from moonraker
+ * @returns {pos} position that kaptain can use
+ */
 export const lambda = (arr: raw): pos => {
     return {
         x: arr[0],
