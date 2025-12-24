2e1554024db1a85f40d0af0ec4e411f361683850
jerren <jerren.schexnider@icloud.com>
2025-12-24 12:30:30 -0500

    houston is up

--- util/parse.ts
+++ util/parse.ts
@@ -1,3 +1,8 @@
+/**
+ * is moonraker sending slop? this determines
+ * @param data - data object from moonraker
+ * @returns boolean - is it slop?
+ */
 export function slop(data: object) {
 	if (!data || data?.["moonraker_stats"]) {
 		return true;
@@ -6,6 +11,10 @@ export function slop(data: object) {
 	}
 }
 
-export function parse(data: string) {
-	return JSON.parse(data)?.["params"]?.[0] ?? null;
-}
+/**
+ * parses the important data from the websocket
+ * @param data - the data string from the ws
+ * @returns object - the data in an object form
+ */
+
+export const parse = (data: string): object => {return JSON.parse(data)?.["params"]?.[0] ?? null;}
